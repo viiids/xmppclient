157f358534dc21329dc70498dfb8d73fc9f14f7d
viiids <viiids@gmail.com>
2013-06-25 23:15:23 -0700

    1. Adding options file. 2. Sample usage of Chrome api.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
 	console.log('XMPP client first log.');
 	// The rabbit hole starts here.
+	
+	// Sample use of Chrome extensions api
+	chrome.browserAction.setTitle({title: 'Xmpp chat'});
 });
